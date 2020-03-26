eacd9913d5f2a090369eee94851d318015bbf4eb
Víctor Martín Molina <vmartin@profile.es>
2020-03-26 08:50:15 +0100

    jacoco

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("groovy")
     kotlin("jvm") version "1.3.70"
+    jacoco
 }
 
 java.sourceCompatibility = JavaVersion.VERSION_1_8
@@ -30,4 +31,16 @@ sourceSets["test"].withConvention(GroovySourceSet::class)  {
 sourceSets["main"].withConvention(GroovySourceSet::class)  {
     groovy.srcDir("src")
     groovy.srcDir("vars")
+}
+
+tasks.jacocoTestReport {
+    reports {
+        xml.isEnabled = true
+        html.destination = file("${buildDir}/jacocoHtml")
+    }
+//    classDirectories.setFrom(
+//            sourceSets.main.get().output.asFileTree.matching {
+//                exclude("xxx/**/*.class")
+//            }
+//    )
 }
